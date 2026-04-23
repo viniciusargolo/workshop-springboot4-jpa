11ba4422e4be2c2ea31ec5be349d1667854e25f1
viniciusargolo <viniciusargolo.0@gmail.com>
2026-04-23 20:46:18 -0300

    h2 database, test profile, jpa

--- src/main/java/com/viniparg/course/resources/UserResource.java
+++ src/main/java/com/viniparg/course/resources/UserResource.java
@@ -2,6 +2,7 @@ package com.viniparg.course.resources;
 
 import com.viniparg.course.entities.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @GetMapping
     public ResponseEntity<User> findAll() {
         User u = new User(1L, "Maria", "maria@email.com", "99999999", "12345");
         return ResponseEntity.ok().body(u);
