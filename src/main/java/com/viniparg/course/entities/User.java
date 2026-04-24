f78480ba3159822465a91e11b7712e1dbb9b134f
viniciusargolo <viniciusargolo.0@gmail.com>
2026-04-24 15:32:51 -0300

    Order, Instannt, Iso 8601 pt 1

--- src/main/java/com/viniparg/course/entities/User.java
+++ src/main/java/com/viniparg/course/entities/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -20,6 +22,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private final List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -71,6 +76,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
