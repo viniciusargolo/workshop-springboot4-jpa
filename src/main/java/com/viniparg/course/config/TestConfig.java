6ee622c74cc0e20cc749c11fd41155c5c4079e1e
viniciusargolo <viniciusargolo.0@gmail.com>
2026-04-24 17:24:26 -0300

    OrderStatus enum

--- src/main/java/com/viniparg/course/config/TestConfig.java
+++ src/main/java/com/viniparg/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.viniparg.course.config;
 
 import com.viniparg.course.entities.Order;
 import com.viniparg.course.entities.User;
+import com.viniparg.course.entities.enums.OrderStatus;
 import com.viniparg.course.repositories.OrderRepository;
 import com.viniparg.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "mariab@email.com", "987654321", "12345");
         User u2 = new User(null, "Alex Green", "alex@email.com", "123456789", "12345");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:27Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-06-20T19:42:37Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-06-20T19:33:17Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:27Z"), OrderStatus.PAID,u1);
+        Order o2 = new Order(null, Instant.parse("2019-06-20T19:42:37Z"), OrderStatus.WAITING_PAYMENT,u2);
+        Order o3 = new Order(null, Instant.parse("2019-06-20T19:33:17Z"), OrderStatus.WAITING_PAYMENT,u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
