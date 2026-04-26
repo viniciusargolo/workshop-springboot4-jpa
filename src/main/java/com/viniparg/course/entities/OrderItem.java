50b4ea7f7c48d71f6670adb798c1421f536a520e
viniciusargolo <viniciusargolo.0@gmail.com>
2026-04-26 05:48:27 -0300

    Subtotal & total methods

--- src/main/java/com/viniparg/course/entities/OrderItem.java
+++ src/main/java/com/viniparg/course/entities/OrderItem.java
@@ -66,6 +66,9 @@ public class OrderItem implements Serializable {
         id.setProduct(product);
     }
 
+    public Double getSubTotal() {
+        return price * quantity;
+    }
 
     @Override
     public boolean equals(Object o) {
