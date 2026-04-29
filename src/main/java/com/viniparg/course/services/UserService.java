71bbe2f0307891fc9a5f2ed3bec53ba7b5677473
viniciusargolo <viniciusargolo.0@gmail.com>
2026-04-29 10:02:32 -0300

    Exception handling - findById

--- src/main/java/com/viniparg/course/services/UserService.java
+++ src/main/java/com/viniparg/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.viniparg.course.services;
 
 import com.viniparg.course.entities.User;
 import com.viniparg.course.repositories.UserRepository;
+import com.viniparg.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
